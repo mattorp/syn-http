b9a4061fefa37e9d4a407aeabdae01af91d3f107
mattorp <matto@oeye.site>
2022-06-02 13:48:39 +0200

    only warn once and notfiy if osc is activated

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import {
 dotenv.config()
 
 let lastFetched = Date.now()
+let warnedAboutFetch = false
 
 httpServer.on('request', async (req, res) => {
-  const [, ...msg] = decodeURI(req.url).split('/')
-  const address = getAddress(msg)
-  const msgValue = getMsgValue(msg)
-
   try {
+    const [, ...msg] = decodeURI(req.url).split('/')
+    const address = getAddress(msg)
+    const msgValue = getMsgValue(msg)
     res.statusCode = 200
     if (getVariant(msg) === 'values') {
       res.end(getValue({ msgValue, res }))
@@ -26,7 +26,7 @@ httpServer.on('request', async (req, res) => {
   } catch (err) {
     console.error(err)
     res.statusCode = 500
-    res.end(err)
+    res.end(err.message)
   }
 })
 
@@ -34,10 +34,16 @@ oscServer
   .on('bundle', (bundle) => {
     storeValues(bundle)
     lastFetched = Date.now()
+    if (warnedAboutFetch) {
+      warnedAboutFetch = false
+      console.log('\nOSC has been reactivated')
+    }
   })
 
 setInterval(() => {
-  if (lastFetched < Date.now() - 2000) {
-    throw new Error('Values are not being received. Check if Synesthesia is running and OSC output is enabled.')
+  if (lastFetched < Date.now() - 2000 &&
+    !warnedAboutFetch) {
+    warnedAboutFetch = true
+    console.error('Values are not being received. Check if Synesthesia is running and OSC output is enabled.')
   }
 }, 2000)
