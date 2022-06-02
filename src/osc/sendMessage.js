0c19360fddfc7a3a821e5c27bc8aa520bbb94fa8
mattorp <matto@oeye.site>
2022-06-02 13:49:55 +0200

    split messages into x,y,r if multiple values

--- src/osc/sendMessage.js
+++ src/osc/sendMessage.js
@@ -2,25 +2,31 @@ import {
   logInfo
 } from '../logging.js'
 const formatValue = (value) =>
-  isNaN(value) ? value : value.split(' ').map(val => parseFloat(val))
+  isNaN(value) ? value : value.split(' ').map((val, i) => parseFloat(val))
+
+const entries = ['x', 'y', 'r']
+
+const splitMessages = (address, value) => {
+  const values = value.split(' ')
+  return values.length === 1 ? [[address, value]] : values.map((val, i) => [address + '/' + entries[i], val])
+}
 
 export const sendMessage = oscClient => async (address, value) => {
   const promise = new Promise((resolve, reject) => {
     try {
-      console.log(formatValue(value))
-      oscClient.send(address, formatValue(value),
-        (err) => {
-          if (err) {
-            console.error(err)
-            reject(err)
-          } else {
-            logInfo(`
-Sent message
-${address}
-${value}`)
-            resolve('OK')
+      splitMessages(address, value).forEach(([addr, val]) => {
+        oscClient.send(addr, formatValue(val),
+          (err) => {
+            if (err) {
+              console.error(err)
+              reject(err)
+            } else {
+              logInfo(`Sent ${addr} ${val}`)
+              resolve('OK')
+            }
           }
-        }
+        )
+      }
       )
     } catch (err) {
       console.error(err)
