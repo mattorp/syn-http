76820bc3131b7d1889d5293fe8a6cbef82ab73f0
mattorp <matto@oeye.site>
2022-06-02 14:42:35 +0200

    convert vectors to strings

--- src/osc/sendMessage.js
+++ src/osc/sendMessage.js
@@ -2,11 +2,29 @@ import {
   logInfo
 } from '../logging.js'
 const formatValue = (value) =>
-  isNaN(value) ? value : value.split(' ').map((val, i) => parseFloat(val))
+  isNaN(value) ? value.split(' ').map((val, i) => parseFloat(val)) : value
 
-const entries = ['x', 'y', 'r']
+const entries = ['r', 'g', 'b']
 
-const splitMessages = (address, value) => value.split(' ').map((val, i, { length }) => [address + (length > 1 ? '/' + entries[i] : ''), val])
+// TODO clean up
+const splitMessages = (address, passedValue) => {
+  // convert arrays to 'r g b'
+  const value = passedValue[0] === '(' || passedValue[0] === '['
+    ? passedValue
+      // remove [], () and spaces
+      .replace(/[\[\]\(\)\s]/g, '')
+      .split(',').map(val => parseFloat(val)).join(' ')
+    : passedValue
+
+  if (
+    address.slice(0, 10) === '/controls/' &&
+    value.slice(0, 3) === 'rgb') {
+    // Normalize color so that (255, 255, 255) is (1, 1, 1)
+    return value.slice(4, -1).split(',').map((val, i) => [address + '/' + entries[i], parseInt(val) / 255])
+  } else {
+    return value.split(' ').map((val, i, { length }) => [address + (length > 1 ? '/' + entries[i] : ''), val])
+  }
+}
 
 export const sendMessage = oscClient => async (address, value) => {
   const promise = new Promise((resolve, reject) => {
