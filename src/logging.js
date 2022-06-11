db51167fab74552b43c597fd9e0639032952f5a4
mattorp <matto@oeye.site>
2022-06-11 14:39:57 +0200

    rename logWarnings to logWarning

--- src/logging.js
+++ src/logging.js
@@ -1,3 +1,3 @@
 export const logInfo = (info) => process.env.LOG_INFO && console.log(info)
 
-export const logWarnings = (warn) => process.env.LOG_WARNINGS && console.log(warn)
+export const logWarning = (warn) => process.env.LOG_WARNINGS && console.log(warn)
